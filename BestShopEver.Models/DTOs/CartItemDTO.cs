b418cc988a7fe2e31bd014c122c7d2f93978ce23
Stanislav Svehlik <stanislav.svehlik@gmail.com>
2023-03-08 18:18:01 +0100

    add dtos classes

---
+++ BestShopEver.Models/DTOs/CartItemDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BestShopEver.Models.DTOs
+{
+    internal class CartItemDto
+    {
+    }
+}
